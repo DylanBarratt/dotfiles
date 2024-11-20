7de43a532ed003e6f53b6dbf35310a35c71c748a
Dylan Barratt <52459348+DylanBarratt@users.noreply.github.com>
2024-11-20 14:49:37 +0000

    Delete nvim/.config/nvim directory

--- nvim/.config/nvim/lua/plugins/katas.lua
+++
@@ -1,7 +0,0 @@
-return {
-  dir = '~/Coding/lua/katas.nvim', -- Your path
-  name = 'katas',
-  config = function()
-    require('katas').setup()
-  end,
-}
--- nvim/.config/nvim/lua/plugins/util/trouble.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    'folke/trouble.nvim',
-    opts = {
-      open_no_results = true,
-    }, -- for default options, refer to the configuration section for custom setup.
-    cmd = 'Trouble',
-  },
-}
