f4e97b631b3b7c239584e7138121512ebe93fd01
Dylan Barratt <dylanbarratt03@gmail.com>
2024-11-24 16:20:29 +0000

    remove local plugin

--- nvim/lua/plugins/local/katas.lua
+++ nvim/lua/plugins/local/katas.lua
@@ -1,7 +1,7 @@
-return {
-  dir = '~/Coding/katas.nvim',
-  name = 'katas',
-  config = function()
-    require('katas').setup()
-  end,
-}
+-- return {
+--   dir = '~/Coding/katas.nvim',
+--   name = 'katas',
+--   config = function()
+--     require('katas').setup()
+--   end,
+-- }
