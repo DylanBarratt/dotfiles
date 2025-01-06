87d61c2bd885a33156de4a5fbea86054608dd3d6
Dylan Barratt <dylan.barratt@wjec.co.uk>
2025-01-06 11:44:31 +0000

    oooooo pretty

---
+++ nvim/lua/plugins/ui/barbecue.lua
@@ -0,0 +1,12 @@
+return {
+  "utilyre/barbecue.nvim",
+  event = "BufEnter",
+  version = "*",
+  dependencies = {
+    "SmiteshP/nvim-navic",
+    "nvim-tree/nvim-web-devicons",
+  },
+  opts = {
+    -- configurations go here
+  },
+}
