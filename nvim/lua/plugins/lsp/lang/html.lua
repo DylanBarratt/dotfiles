68bd72c92c6ece693190a515527a9b25b7bc0f1d
Dylan Barratt <dylan.barratt@wjec.co.uk>
2025-01-06 11:46:44 +0000

    better names ok

---
+++ nvim/lua/plugins/lsp/lang/html.lua
@@ -0,0 +1,10 @@
+return {
+  { -- auto complete html tags
+    "windwp/nvim-ts-autotag",
+    ft = {
+      "html",
+      "typescriptreact",
+    }, -- NOTE: add any other langs that use html tags here
+    opts = {},
+  },
+}
--- nvim/lua/plugins/lsp/lang/html_autotag.lua
+++
@@ -1,8 +0,0 @@
-return { -- auto complete html tags
-  "windwp/nvim-ts-autotag",
-  ft = {
-    "html",
-    "typescriptreact",
-  }, -- NOTE: add any other langs that use html tags here
-  opts = {},
-}
