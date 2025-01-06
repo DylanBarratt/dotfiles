fa14efb9ce69b502a0f9eb9369e53cc60bc79aaa
Dylan Barratt <dylan.barratt@wjec.co.uk>
2025-01-06 15:31:21 +0000

    better lsp goto

---
+++ nvim/lua/plugins/lsp/glance.lua
@@ -0,0 +1,12 @@
+return {
+  "dnlhc/glance.nvim",
+  event = "LspAttach",
+  cmd = "Glance",
+  opts = {},
+  keys = {
+    { "gd", "<CMD>Glance definitions<CR>", mode = "n", desc = "" },
+    { "gR", "<CMD>Glance references<CR>", mode = "n", desc = "" },
+    { "gy", "<CMD>Glance type_definitions<CR>", mode = "n", desc = "" },
+    { "gm", "<CMD>Glance implementations<CR>", mode = "n", desc = "" },
+  },
+}
