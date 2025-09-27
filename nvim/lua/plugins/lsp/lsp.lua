1447e1208914dbad2720790db3d793c1a65c9413
Dylan Barratt <dylan.barratt@wjec.co.uk>
2025-09-27 15:29:28 +0100

    chore: removed deprecated setup

--- nvim/lua/plugins/lsp/lsp.lua
+++ nvim/lua/plugins/lsp/lsp.lua
@@ -17,22 +17,5 @@ return { -- lsp server setup
       ensure_installed = vim.tbl_keys(opts.servers),
       automatic_installation = true,
     })
-
-    require("mason-lspconfig").setup_handlers({
-      function(server_name)
-        -- Check if the server is disabled
-        local server_opts = opts.servers[server_name]
-        if server_opts and server_opts.enabled == false then
-          return -- skip setup for this server
-        end
-
-        local capabilities = require("blink.cmp").get_lsp_capabilities()
-
-        require("lspconfig")[server_name].setup({
-          settings = server_opts,
-          capabilities = capabilities,
-        })
-      end,
-    })
   end,
 }
