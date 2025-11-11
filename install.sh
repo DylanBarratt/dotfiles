e11aebcd3ce5561f31de09c0cfb517d5e96b0fff
Dylan Barratt <dylan.barratt@wjec.co.uk>
2025-11-11 16:07:06 +0000

    more stuff

--- install.sh
+++ install.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
 
+installMsg() {
+  echo "--------------------------------"
+  echo "INSTALLING $1"
+  echo "--------------------------------"
+}
+
 # INSTALL STUFF!
 (
+  installMsg("STUFF")
   # move to tmp so any setup files installed are not in the way
   cd /tmp
 
   apt update
 
   # fd find
-  apt install fd-find
+  installMsg("fd")
+  apt install -y fd-find
 
   # zsh
-  apt install zsh
+  installMsg("zsh")
+  apt install -y zsh
   chsh -s $(which zsh) # change default shell to zsh
 
   # Lua:
+  installMsg("lua")
   (
-    apt-get install libreadline-dev
+    apt-get install -y libreadline-dev
     curl -L -R -O https://www.lua.org/ftp/lua-5.1.tar.gz
     tar zxf lua-5.1.tar.gz
     cd lua-5.1
@@ -26,6 +36,7 @@
   )
 
   # luarocks
+  installMsg("luarocks")
   (
     wget https://luarocks.org/releases/luarocks-3.11.1.tar.gz
     tar zxpf luarocks-3.11.1.tar.gz
@@ -36,6 +47,7 @@
   )
 
   # fzf
+  installMsg("fzf")
   git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
   ~/.fzf/install
 )
