5a3c0726bb2b939e7f75d118a595ce09cb23b6db
Dylan Barratt <dylan.barratt@wjec.co.uk>
2025-11-11 16:14:28 +0000

    fixed functions

--- install.sh
+++ install.sh
@@ -8,23 +8,23 @@ installMsg() {
 
 # INSTALL STUFF!
 (
-  installMsg("STUFF")
+  installMsg "STUFF"
   # move to tmp so any setup files installed are not in the way
   cd /tmp
 
   apt update
 
   # fd find
-  installMsg("fd")
+  installMsg "fd"
   apt install -y fd-find
 
   # zsh
-  installMsg("zsh")
+  installMsg "zsh"
   apt install -y zsh
   chsh -s $(which zsh) # change default shell to zsh
 
   # Lua:
-  installMsg("lua")
+  installMsg "lua"
   (
     apt-get install -y libreadline-dev
     curl -L -R -O https://www.lua.org/ftp/lua-5.1.tar.gz
@@ -36,7 +36,7 @@ installMsg() {
   )
 
   # luarocks
-  installMsg("luarocks")
+  installMsg "luarocks"
   (
     wget https://luarocks.org/releases/luarocks-3.11.1.tar.gz
     tar zxpf luarocks-3.11.1.tar.gz
@@ -47,7 +47,7 @@ installMsg() {
   )
 
   # fzf
-  installMsg("fzf")
+  installMsg "fzf"
   git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
   ~/.fzf/install
 )
