97bf334686520cffd3fb69d3a9291e34ad9aae80
Dylan Barratt <dylan.barratt@wjec.co.uk>
2025-12-05 15:55:32 +0000

    corrected list

--- install.sh
+++ install.sh
@@ -99,7 +99,8 @@ chmod -R +x ~/.config/gitCommands
 
 # copy devcontainer workspace(s) to home in worktree format i like :)
 if [ -d "/workspaces" ]; then
-  for folder in $(echo /workspaces/*); do
+  cd /workspaces || return
+  for folder in $(echo */); do
     echo "duplicating $folder"
     mkdir "$HOME/$folder"
     cp -r "/workspaces/$folder" "$HOME/$folder/master"
